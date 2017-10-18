d43562ff2b7a09c85cd61c3e350170d8a8893008
decidedlygray <dg@castle.neomailbox.ch>
2017-10-18 18:18:24 -0500

    Enhancement for Issue 3: Adding ARM build support

---
+++ buildarm.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+export ARCH=arm
+TARGET=$1
+
+# Sets up toolchain environment variables for arm toolchain
+# Tested against armv7-eabihf, glibc from toolchains.free-electrons.com
+
+warn()
+{
+	echo "$1" >&2
+}
+
+if [ ! -z $(which arm-linux-gcc-5.4.0) ];
+then
+	export CC=$(which arm-linux-gcc-5.4.0)
+else
+	warn "Not setting CC: can't locate arm-linux-gcc-5.4.0."
+fi
+
+if [ ! -z $(which arm-linux-ld) ];
+then
+	export LD=$(which arm-linux-ld)
+else
+	warn "Not setting LD: can't locate arm-linux-ld."
+fi
+
+if [ ! -z $(which arm-linux-ar) ];
+then
+	export AR=$(which arm-linux-ar)
+else
+	warn "Not setting AR: can't locate arm-linux-ar."
+fi
+
+
+if [ ! -z $(which arm-linux-strip) ];
+then
+	export STRIP=$(which arm-linux-strip)
+else
+	warn "Not setting STRIP: can't locate arm-linux-strip."
+fi
+
+if [ ! -z $(which arm-linux-nm) ];
+then
+	export NM=$(which arm-linux-nm)
+else
+	warn "Not setting NM: can't lcoate arm-linux-nm."
+fi
+
+
+make $TARGET || exit $?
